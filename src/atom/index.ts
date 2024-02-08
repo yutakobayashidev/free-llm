818515b83badec4902d47c5f8a2a0725e90ba575
yutakobaayshidev <91340399+yutakobayashidev@users.noreply.github.com>
2024-02-08 16:50:24 +0900

    Update systemPromptAtom value

--- src/atom/index.ts
+++ src/atom/index.ts
@@ -2,5 +2,5 @@ import { atomWithStorage } from "jotai/utils";
 
 export const apiKeyAtom = atomWithStorage("apiKey", "");
 
-export const systemPromptAtom = atomWithStorage("systemPrompt", "null");
+export const systemPromptAtom = atomWithStorage("systemPrompt", "");
 export const modelAtom = atomWithStorage("model", "gpt-3.5-turbo");
