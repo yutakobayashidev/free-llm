ca6cb9e9da9b31063f6cad19584b87529c9cc261
yutakobaayshidev <91340399+yutakobayashidev@users.noreply.github.com>
2024-02-12 04:07:35 +0900

    Add pagination component and implement pagination functionality

--- src/app/(main)/hub/page.tsx
+++ src/app/(main)/hub/page.tsx
@@ -1,11 +1,50 @@
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
 import { db } from "@/db/client";
 import * as schema from "@/db/schema";
 import clsx from "clsx";
-import { desc, eq, or } from "drizzle-orm";
+import { desc, eq, or, sql } from "drizzle-orm";
 import { MessageSquare } from "lucide-react";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
-export default async function Page() {
+interface SearchParamsProps {
+  searchParams: {
+    page: string;
+  };
+}
+
+export default async function Page({ searchParams }: SearchParamsProps) {
+  const pageNumber = Number(searchParams.page ?? 1);
+
+  const numberOfItems = 6;
+
+  const totalChats = await db
+    .select({ count: sql<number>`count(*)` })
+    .from(schema.chats)
+    .where(or(eq(schema.chats.publishStatus, "public"), eq(schema.chats.publishStatus, "guild")));
+
+  const numberOfPages = Math.ceil(totalChats[0].count / numberOfItems);
+
+  let safePageNumber = 1;
+
+  if (pageNumber < 1) {
+    redirect("/hub");
+  } else if (pageNumber > numberOfPages) {
+    redirect("/hub");
+  } else {
+    safePageNumber = pageNumber;
+  }
+
+  const offsetItems = safePageNumber > 1 ? (safePageNumber - 1) * numberOfItems : 0;
+
   const chats = await db.query.chats.findMany({
     where: or(eq(schema.chats.publishStatus, "public"), eq(schema.chats.publishStatus, "guild")),
     orderBy: [desc(schema.chats.createdAt)],
@@ -13,12 +52,37 @@ export default async function Page() {
       user: true,
       messages: true,
     },
+    offset: offsetItems,
+    limit: 10,
   });
+
+  const prevSearchParams = new URLSearchParams();
+  const nextSearchParams = new URLSearchParams();
+
+  if (safePageNumber > 2) {
+    prevSearchParams.set("page", `${safePageNumber - 1}`);
+  } else {
+    prevSearchParams.delete("page");
+  }
+
+  if (safePageNumber > 0) {
+    if (safePageNumber === numberOfPages) {
+      nextSearchParams.set("page", `${numberOfPages}`);
+    } else {
+      nextSearchParams.set("page", `${safePageNumber + 1}`);
+    }
+  } else {
+    nextSearchParams.delete("page");
+  }
+
+  const startPage = Math.max(1, safePageNumber - 1);
+  const endPage = Math.min(numberOfPages, safePageNumber + 1);
+
   return (
     <div className="main-content w-full overflow-y-auto py-8 px-10">
       <h1 className="text-2xl mb-5 font-bold leading-tight tracking-tighter md:text-4xl lg:leading-[1.1]">Message Hub</h1>
       <p className="mb-5">公開・サーバーの参加者限定のチャット一覧</p>
-      <div className="space-y-5">
+      <div className="space-y-5 mb-5">
         {chats.map((chat) => (
           <Link href={`/share/${chat.id}`} className="flex justify-between items-center" key={chat.id}>
             <div className="gap-x-3 flex items-center">
@@ -50,6 +114,32 @@ export default async function Page() {
             </div>
           </Link>
         ))}
+      </div>
+      <div>
+        <Pagination className="flex justify-end">
+          <PaginationContent>
+            {safePageNumber > 1 && (
+              <PaginationItem>
+                <PaginationPrevious href={`/hub?${prevSearchParams}`} />
+              </PaginationItem>
+            )}
+            {[...Array(endPage - startPage + 1)].map((_, index) => {
+              const page = startPage + index;
+              return (
+                <PaginationItem key={page}>
+                  <PaginationLink href={`/hub?page=${page}`} className={clsx({ "bg-primary text-white": page === safePageNumber })}>
+                    {page}
+                  </PaginationLink>
+                </PaginationItem>
+              );
+            })}
+            {safePageNumber < numberOfPages && (
+              <PaginationItem>
+                <PaginationNext href={`/hub?${nextSearchParams}`} />
+              </PaginationItem>
+            )}
+          </PaginationContent>
+        </Pagination>
       </div>
     </div>
   );
